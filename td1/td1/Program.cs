02512ec9a86e8be51b5f776e5244a895cc7ec722
MaloBardin <105814710+MaloBardin@users.noreply.github.com>
2024-03-12 14:30:42 +0100

    bitmap
    debut bitmap

--- td1/td1/Program.cs
+++ td1/td1/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace td1
+﻿using System.Drawing;
+
+
+namespace td1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Bitmap d = new Bitmap(200,100);
         }
     }
 }
