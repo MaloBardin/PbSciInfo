f98962e834e1b67117701faf2d1942f8f751d1c6
daemonmx <65545516+daemonmx@users.noreply.github.com>
2024-03-12 15:52:36 +0100

    création classse PIXEL

---
+++ td1/td1/PIXEL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace td1
+{
+    
+
+        public class Pixel
+        {
+            byte red;
+            byte green;
+            byte blue;
+
+            public Pixel(byte r, byte g, byte b)
+            {
+                this.red = r;
+                this.green = g;
+                this.blue = b;
+            }
+            public int R
+            {
+                get { return red; }
+            }
+            public int G
+            {
+                get { return green; }
+            }
+            public int B
+            {
+                get { return blue; }
+            }
+            public string toString()
+            {
+                return "R" + red + " G" + green + " B" + blue;
+            }
+
+
+        }
+    }
+
