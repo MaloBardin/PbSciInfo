115248f4bcd94e6c22f4ee909a1013e1cfa9904b
PlayboiMagnus <theo.serene@gmail.com>
2024-03-12 16:23:45 +0100

    Changer gris
    Classe pour changer un pixel en nuance de gris

---
+++ td1/td1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace td1
+{
+    internal class NuancesGris
+    {
+        Pixel couleur;
+
+        public NuancesGris(Pixel couleur)
+        {
+            this.couleur = couleur;
+        }
+        public Pixel PixelEnGris(Pixel couleur)
+        {
+            Pixel gris = couleur;
+            int valeur = (couleur.R + couleur.G + couleur.B)/3;
+            gris.R = valeur;
+            gris.G = valeur;
+            gris.B = valeur;
+            return gris;
+        }
+
+    }
+}
