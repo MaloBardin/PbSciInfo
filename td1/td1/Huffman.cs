a1bc46bdb3a05113ed6667e8531b754daaecd725
daemonmx <65545516+daemonmx@users.noreply.github.com>
2024-04-29 01:02:22 +0200

    huffman noeud update

--- td1/td1/Huffman.cs
+++ td1/td1/Huffman.cs
@@ -8,5 +8,60 @@ namespace td1
 {
     internal class Huffman
     {
+        private Noeud racine;
+        private Dictionary<int, Pixel> dico;
+
+        public Huffman(Noeud racine)
+        {
+            this.racine = racine;
+            dico = new Dictionary<int, Pixel>();
+            Parcours(racine, "");
+        }
+
+        private void Parcours(Noeud n, string s)
+        {
+            if (n.EstFeuille())
+            {
+                dico.Add(n.Valeur, s);
+            }
+            else
+            {
+                Parcours(n.Gauche, s + "0");
+                Parcours(n.Droit, s + "1");
+            }
+        }
+
+        public string Encodage(byte[] data)
+        {
+            string res = "";
+            foreach (byte b in data)
+            {
+                res += dico[b];
+            }
+            return res;
+        }
+
+        public byte[] Decodage(string data)
+        {
+            List<byte> res = new List<byte>();
+            Noeud n = racine;
+            foreach (char c in data)
+            {
+                if (c == '0')
+                {
+                    n = n.Gauche;
+                }
+                else
+                {
+                    n = n.Droit;
+                }
+                if (n.EstFeuille())
+                {
+                    res.Add(n.Valeur);
+                    n = racine;
+                }
+            }
+            return res.ToArray();
+        }
     }
 }
--- td1/td1/Noeud.cs
+++ td1/td1/Noeud.cs
@@ -8,5 +8,51 @@ namespace td1
 {
     internal class Noeud
     {
-    }
+        private int frequence;
+        private Noeud gauche;
+        private Noeud droit;
+        private Pixel pix;
+
+        public Noeud(int frequence, Pixel pix)
+        {
+            this.frequence = frequence;
+            this.pix = pix;
+        }
+
+        public Noeud(int frequence, Noeud gauche, Noeud droit)
+        {
+            this.frequence = frequence;
+            this.gauche = gauche;
+            this.droit = droit;
+        }
+
+        public int Frequence
+        {
+            get { return frequence; }
+            set { frequence = value; }
+        }
+
+        public Noeud Gauche
+        {
+            get { return gauche; }
+            set { gauche = value; }
+        }
+
+        public Noeud Droit
+        {
+            get { return droit; }
+            set { droit = value; }
+        }
+        
+        public Pixel Pix
+        {
+            get { return pix; }
+            set { pix = value; }
+        }
+
+        public bool EstFeuille()
+        {
+            return gauche == null && droit == null;
+        }
+
 }
