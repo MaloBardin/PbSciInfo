9f478ac05e42968a34793a4f53f49930ac59af43
PlayboiMagnus <152646957+PlayboiMagnus@users.noreply.github.com>
2024-04-29 17:39:08 +0200

    Ajout de Test
    Test ajouté

---
+++ td1/ProjetTD1.Test/TD1Test.cs
@@ -0,0 +1,16 @@
+
+using System.Drawing;
+
+namespace ProjetTD1.Test
+{
+    [TestClass]
+    public class TD1Test
+    {
+
+        [TestMethod]
+        public void Creation_image()
+        {
+            var image = new Image();
+        }
+    }
+}
